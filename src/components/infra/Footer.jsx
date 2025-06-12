7ec4a4e758ddfaa6febe53be2e3c69521789a0d2
Raziel <raziel@za9.com.br>
2025-06-12 09:45:17 -0300

    feat: :sparkles: componentização do footer

---
+++ src/components/infra/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+  return (
+    <footer className="bg-blue-500 text-white text-center p-4">
+      <p>&copy; 2025 - Todos os direitos reservados</p>
+    </footer>
+  );
+}
