7e064ebf4bdba994cf08ba8c69d45f512f0c13b7
Raziel <raziel@za9.com.br>
2025-06-12 09:16:21 -0300

    feat: :sparkles: Adição de inputs para interação do usuário

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,58 @@
+'use client'
+
+import SelectState from "@/components/inputs/SelectState";
+
 export default function Home() {
   return (
     <>
-      <main className="bg-amber-200 min-h-screen p-4">
-        <section className="bg-black text-white p-4 rounded-lg shadow">
-          <h2 className="text-xl font-bold mb-2">Seção de Teste</h2>
-          <p>Este é um conteúdo de teste dentro de uma seção.</p>
+      <main className="bg-blue-300 min-h-screen p-6 flex justify-center items-start">
+        <section className="bg-white text-black p-6 rounded-lg shadow-md w-full max-w-lg">
+          <div className="mb-4">
+            <h2 className="text-2xl font-bold mb-2">Criar Benchmark</h2>
+            <p className="text-gray-600">Preencha os campos abaixo, especificando o período que gostaria de comparar</p>
+          </div>
+
+          <form className="space-y-4">
+
+            <div>
+              <label className="block mb-1 font-medium">Nome do Benchmark</label>
+              <input
+                placeholder="Ex: Comparativo COVID"
+                className="w-full border border-blue-500 rounded px-4 py-2"
+                type="text"
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 font-medium">Data de Início</label>
+              <input
+                className="w-full border border-blue-500 rounded px-4 py-2"
+                type="text"
+                placeholder="DD/MM/AAAA"
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => {
+                  if (!e.target.value) e.target.type = "text";
+                }}
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 font-medium">Data de Término</label>
+              <input
+                className="w-full border border-blue-500 rounded px-4 py-2"
+                type="text"
+                placeholder="DD/MM/AAAA"
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => {
+                  if (!e.target.value) e.target.type = "text";
+                }}
+              />
+            </div>
+
+            <SelectState label="Estado 1" />
+
+            <SelectState label="Estado 2" />
+          </form>
         </section>
       </main>
 
