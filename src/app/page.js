363078519b206a771ed6c18b0aa3924bab780c6b
Raziel <raziel@za9.com.br>
2025-06-11 22:19:41 -0300

    style: :art: Header adicionado a Layout e retirado da page home

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,6 @@
 export default function Home() {
   return (
     <>
-      <header className="flex flex-row justify-between bg-blue-500 w-full h-16 items-center">
-        <h1 className="font-bold text-3xl text-white ml-4 rounded-xl bg-blue-400 p-2">
-          Covid BenchMark Planisa
-        </h1>
-        <nav>
-          <ul className="flex flex-row gap-2 rounded-xl p-2 h-12 items-center text-white hover:cursor-pointer font-bold">
-            <li className="border-r p-2 border-slate-400 hover:text-slate-500">
-              BenchMark
-            </li>
-            <li className="hover:text-slate-500 p-2">Resultados</li>
-          </ul>
-        </nav>
-      </header>
-
       <main className="bg-amber-200 min-h-screen p-4">
         <section className="bg-black text-white p-4 rounded-lg shadow">
           <h2 className="text-xl font-bold mb-2">Seção de Teste</h2>
